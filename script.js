1fd8326b55bf708700b9b40cfda6dd5bab070725
chen_azhuras <muhnaufal229@gmail.com>
2025-04-11 00:03:33 +0700

    apa ajaa

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
+// Fungsi untuk memulai musik
 function playMusic() {
   const music = document.getElementById('background-music');
-  music.muted = false;  // buka mute setelah interaksi
   music.play();
 }
-
-document.body.addEventListener('click', playMusic, { once: true });
-
+window.addEventListener('DOMContentLoaded', function() {
+  playMusic();
+});
 const content = document.getElementById('content');
 const footer = document.getElementsByTagName('footer')[0];
 const timer = document.getElementById('timer');
 
+const second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+let countDown = new Date('Oct 22, 2023 00:00:00').getTime(),
+  x = setInterval(function () {
+    let now = new Date().getTime(),
+      distance = countDown - now;
+    // document.getElementById('days').innerText = Math.floor(distance / (day)),
+    document.getElementById('hours').innerText = Math.floor(distance / (hour)),
+      document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+      document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+    if (distance < 0) {
+
+      timer.classList.add('d-none');
+      confetti();
+      clearInterval(x);
+      _slideSatu();
+    }
+
+  }, second)
+  
 const _slideSatu = function () {
   const tap = document.getElementById('tap');
   const slideSatu = document.getElementById('slideSatu');
